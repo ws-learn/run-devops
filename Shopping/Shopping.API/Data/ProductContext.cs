e06dd6e69070938fc9d4842b47cbca5fc07d0df0
diduboyz <wlsn_ting520@hotmail.com>
2025-09-07 20:18:38 +0800

    change pod replicas

--- Shopping/Shopping.API/Data/ProductContext.cs
+++ Shopping/Shopping.API/Data/ProductContext.cs
@@ -32,7 +32,7 @@ public class ProductContext
             {
                 new Product()
                 {
-                    Name = "IPhone X",
+                    Name = "IPhone 16",
                     Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless. =)",
                     ImageFile = "product-1.png",
                     Price = 950.00M,
